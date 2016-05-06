051b20c3b0307179f2091855187336767964673d
drkchiloll <samwomack_@me.com>
2016-05-06 16:15:38 -0500

    Demoed Enums and more advanced ways to declare functions (with parameters of a certain type and return types or void), declare Arrays using : string[] and or Array<string> (generic type)

--- app4.ts
+++ app4.ts
@@ -1,10 +1,17 @@
 function GetAllBooks() {
-  let books = [
-    {title: 'Ulysses', author: 'James Joyce', availble: true},
-    {title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false},
-    {title: 'I know Why the Caged Bird Sings', author: 'Maya Angelou', available: true},
-    {title: 'Moby Dick', author: 'Herman Melville', available: true}
-  ];
+  let books = [{
+    title: 'Ulysses', author: 'James Joyce',
+    availble: true, category: Category.Fiction
+  }, {
+    title: 'A Farewell to Arms', author: 'Ernest Hemingway',
+    available: false, category: Category.Fiction
+  },{
+    title: 'I know Why the Caged Bird Sings', author: 'Maya Angelou',
+    available: true, category: Category.Poetry
+  }, {
+    title: 'Moby Dick', author: 'Herman Melville',
+    available: true, category: Category.Fiction
+  }];
   return books;
 }
 
@@ -22,5 +29,26 @@ function LogFirstAvailable(books): void {
   console.log('First Available: ' + firstAvailable);
 }
 
-const allBooks = GetAllBooks();
-LogFirstAvailable(allBooks);
+enum Category {Biography, Poetry, Fiction, History, Children}
+
+function GetBookTitlesByCategory(categoryFilter: Category): Array<string> {
+  console.log('Getting books in category: ' + Category[categoryFilter]);
+  const allBooks = GetAllBooks();
+  const filteredTitles: string[] = [];
+  for(let currentBook of allBooks) {
+    if(currentBook.category === categoryFilter) {
+      filteredTitles.push(currentBook.title);
+    }
+  }
+  return filteredTitles;
+}
+
+function LogBookTitles(titles: string[]): void {
+  for(let title of titles) {
+    console.log(title);
+  }
+}
+// const allBooks = GetAllBooks();
+// LogFirstAvailable(allBooks);
+const poetryBooks = GetBookTitlesByCategory(Category.Poetry);
+LogBookTitles(poetryBooks);
